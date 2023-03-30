ef551bc7f5761cdfe59d87362c834deb514d2e92
antohaot <otanton2006@gmail.com>
2023-03-30 23:06:23 +0300

    HW3_python

--- 1.py
+++
@@ -1,8 +0,0 @@
-def can_eat(a, b):
-    dr = abs(a[0] - b[0]), abs(a[1] - b[1])
-    return dr in ((1, 2), (2, 1))
- 
-k = (4, 4)
-from itertools import product
-for i, c in enumerate(product(range(8), repeat=2), 1):
-    print('♞◯⛒'[(k!=c)+can_eat(k,c)], end=' \n'[i%8==0])
