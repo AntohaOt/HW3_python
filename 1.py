df7a05d714ccb3200633483007263841a4ebe341
antohaot <otanton2006@gmail.com>
2023-04-02 23:59:59 +0300

    HW3_python

--- 1.py
+++ 1.py
@@ -1,8 +1,5 @@
-def can_eat(a, b):
-    dr = abs(a[0] - b[0]), abs(a[1] - b[1])
-    return dr in ((1, 2), (2, 1))
+def can_eat(x, y):
+     
+     abs(x[0] - y[0]), abs(x[1] - y[1])
+    
  
-k = (4, 4)
-from itertools import product
-for i, c in enumerate(product(range(8), repeat=2), 1):
-    print('♞◯⛒'[(k!=c)+can_eat(k,c)], end=' \n'[i%8==0])
--- 6.py
+++ 6.py
@@ -1,22 +1,14 @@
 # Создаем переменную 'add':
 add = int(input())
-
 # Создаем переменные 'x' и 'y', которые будут отвечать за координаты:
 index1 = 0
 index2 = 0
+# Создаем список 'arr':
+arr = list(map(int, input().split()))
+# Создаем функию IsSymmetric:
 
-def is_symmetric(a):
-    for x in range(add):
-        for y in range(add):
-            if a[index1][index2] == a[index2][index1]:
-                return True
-    return False
- 
-
-arr = [list(map(int, input().split())) for i in range(add)]
-
-if is_symmetric(arr):
-    
-    print('YES')
-else:
+if index1 == index2:
     print('NO')
+# Иначе - выводим ответ выводим 'YES' 
+else:
+    print('YES')
