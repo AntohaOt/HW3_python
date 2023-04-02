aabfcdb44ee61ff6a11ef0614421d466ab1bddab
antohaot <otanton2006@gmail.com>
2023-04-02 23:48:57 +0300

    HW3_python

--- 6.py
+++ 6.py
@@ -1,10 +1,22 @@
-n=int(input())
-a=[input().split() for i in range(n)]
-ans="YES"
-for i in range(n):
-    for j in range(i+1,n):
-        if a[i][j]!= a[j][i]:
-            ans="NO"
-print(ans)
+# Создаем переменную 'add':
+add = int(input())
 
+# Создаем переменные 'x' и 'y', которые будут отвечать за координаты:
+index1 = 0
+index2 = 0
 
+def is_symmetric(a):
+    for x in range(add):
+        for y in range(add):
+            if a[index1][index2] == a[index2][index1]:
+                return True
+    return False
+ 
+
+arr = [list(map(int, input().split())) for i in range(add)]
+
+if is_symmetric(arr):
+    
+    print('YES')
+else:
+    print('NO')
